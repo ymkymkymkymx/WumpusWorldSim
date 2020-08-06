59c4371e6f6774bf30adb1674d744d4c61c10a7a
Mitesh <miteshkumarca@gmail.com>
2020-08-06 01:42:16 -0400

    observer interface

---
+++ observer/observer.py
@@ -0,0 +1,28 @@
+
+class Observer: 
+
+    def update(visible_board, robot_position, messages, hasArrow, hasGold, orientation):
+        """functtion(visible_board, robot_position, messages, hasArrow, hasGold) -> void"""
+
+
+        """
+            
+            This is the data that the controller provides to the front-end. 
+
+            visible_board : [n*[m*set()]] where each set any of the following strings: 
+                LiveWumpus, DeadWumpus, Stench, Glitter, Breeze, Pit, Arrow, Gold
+
+            robot_position: robot_position[0] = row number(vertical) zero indexed
+                            robot_position[1] = colum number(horizontal) zero indexed
+
+            hasArrow: boolean
+
+            hasGold: boolean
+            
+            orientation: [0,  1]  -> facing right
+                         [0, -1]  -> facing left
+                         [1,  0]  -> facing up
+                         [-1, 0]  -> facing down
+        """
+        
+        pass 

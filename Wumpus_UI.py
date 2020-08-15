8716a0d690e450c62a383e365bc0a792808826f8
Qiao <magika0w0@gmail.com>
2020-08-14 22:57:18 -0400

    add image resources

---
+++ Wumpus_UI.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.title('start')
+    window.geometry('350x250')
+    label_start_title = tk.Label(window, text = 'test')
+    window.mainloop()
